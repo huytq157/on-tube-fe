53165b86ac7dad453025f39fc7e732f3ca9af061
tqhuy157 <huytran.dev157@gmail.com>
2024-09-18 16:43:12 +0700

    fix build

--- src/redux/features/authSlice.ts
+++ src/redux/features/authSlice.ts
@@ -1,43 +1,3 @@
-// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
-// interface UserInfo {
-//   id: string;
-//   name: string;
-//   email: string;
-// }
-
-// interface AuthState {
-//   userInfo: UserInfo | null;
-// }
-
-// const initialState: AuthState = {
-//   userInfo: localStorage.getItem("userInfo")
-//     ? JSON.parse(localStorage.getItem("userInfo") as string)
-//     : null,
-// };
-
-// const authSlice = createSlice({
-//   name: "auth",
-//   initialState,
-//   reducers: {
-//     setCredentials: (state: AuthState, action: PayloadAction<UserInfo>) => {
-//       state.userInfo = action.payload;
-//       localStorage.setItem("userInfo", JSON.stringify(action.payload));
-
-//       const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000;
-//       localStorage.setItem("expirationTime", expirationTime.toString());
-//     },
-
-//     logOut: (state: AuthState) => {
-//       state.userInfo = null;
-//       localStorage.clear();
-//     },
-//   },
-// });
-
-// export const { setCredentials, logOut } = authSlice.actions;
-// export default authSlice.reducer;
-
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface AuthState {
@@ -45,7 +5,9 @@ interface AuthState {
 }
 
 const initialState: AuthState = {
-  token: localStorage.getItem("authToken") || null,
+  token:
+    (typeof window !== "undefined" && localStorage.getItem("authToken")) ||
+    null,
 };
 
 const authSlice = createSlice({
